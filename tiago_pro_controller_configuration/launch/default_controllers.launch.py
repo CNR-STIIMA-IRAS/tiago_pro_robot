c8eaa2195ad3fe10baade95f884e73145d4bb4e8
ileniaperrella <ilenia.perrella@pal-robotics.com>
2024-02-07 14:11:57 +0100

    integrate mobile_base_controller

--- tiago_pro_controller_configuration/launch/default_controllers.launch.py
+++ tiago_pro_controller_configuration/launch/default_controllers.launch.py
@@ -25,7 +25,28 @@ def generate_launch_description():
 
     pkg_share_folder = get_package_share_directory(
         'tiago_pro_controller_configuration')
+    
+    # Mobile base controller
+    default_config = os.path.join(
+        pkg_share_folder,
+        'config', 'mobile_base_controller.yaml')
 
+    calibration_config = '/etc/calibration/master_calibration.yaml'
+
+    if os.path.exists(calibration_config):
+        params_file = merge_param_files([default_config, calibration_config])
+    else:
+        params_file = default_config
+
+    mobile_base_controller_launch = GroupAction(
+        [generate_load_controller_launch_description(
+            controller_name='mobile_base_controller',
+            controller_type='omni_drive_controller/OmniDriveController',
+            controller_params_file=params_file)
+         ],
+        forwarding=False)
+
+    
     joint_state_broadcaster_launch = GroupAction(
         [generate_load_controller_launch_description(
             controller_name='joint_state_broadcaster',
@@ -101,8 +122,9 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     ld.add_action(get_robot_name('tiago_pro'))
-
+    
     ld.add_action(joint_state_broadcaster_launch)
+    ld.add_action(mobile_base_controller_launch)
     ld.add_action(torso_controller_launch)
     ld.add_action(head_controller_launch)
     ld.add_action(arm_right_controller_launch)
