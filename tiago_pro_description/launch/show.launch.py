ff388a3bde852573c6669ff0a32f52eafbcbde9a
David ter Kuile <david.terkuile@pal-robotics.com>
2024-03-14 18:37:20 +0100

    Restructure URDF

--- tiago_pro_description/launch/show.launch.py
+++ tiago_pro_description/launch/show.launch.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 PAL Robotics S.L. All rights reserved.
+# Copyright (c) 2022 PAL Robotics S.L. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,41 +12,93 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from launch import LaunchDescription
-from launch_pal.include_utils import include_launch_py_description
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
-def generate_launch_description():
+from launch_pal.include_utils import include_scoped_launch_py_description
+from launch_pal.arg_utils import LaunchArgumentsBase, CommonArgs
+from launch_pal.robot_arguments import TiagoProArgs
 
-    robot_state_publisher = include_launch_py_description(
-        'tiago_pro_description', ['launch', 'robot_state_publisher.launch.py'])
+from dataclasses import dataclass
 
-    start_joint_pub_gui = Node(
+
+@dataclass(frozen=True)
+class LaunchArguments(LaunchArgumentsBase):
+    base_type: DeclareLaunchArgument = TiagoProArgs.base_type
+    arm_type_right: DeclareLaunchArgument = TiagoProArgs.arm_type_right
+    arm_type_left: DeclareLaunchArgument = TiagoProArgs.arm_type_left
+    end_effector_right: DeclareLaunchArgument = TiagoProArgs.end_effector_right
+    end_effector_left: DeclareLaunchArgument = TiagoProArgs.end_effector_left
+    ft_sensor_right: DeclareLaunchArgument = TiagoProArgs.ft_sensor_right
+    ft_sensor_left: DeclareLaunchArgument = TiagoProArgs.ft_sensor_left
+    wrist_model_right: DeclareLaunchArgument = TiagoProArgs.wrist_model_right
+    wrist_model_left: DeclareLaunchArgument = TiagoProArgs.wrist_model_left
+    camera_model: DeclareLaunchArgument = TiagoProArgs.camera_model
+    laser_model: DeclareLaunchArgument = TiagoProArgs.laser_model
+    use_sim_time: DeclareLaunchArgument = CommonArgs.use_sim_time
+    namespace: DeclareLaunchArgument = CommonArgs.namespace
+
+
+def declare_actions(launch_description: LaunchDescription, launch_args: LaunchArguments):
+
+    robot_state_publisher = include_scoped_launch_py_description(
+        pkg_name='tiago_pro_description',
+        paths=['launch', 'robot_state_publisher.launch.py'],
+        launch_arguments={'arm_type_right': launch_args.arm_type_right,
+                          'arm_type_left': launch_args.arm_type_left,
+                          "end_effector_right": launch_args.end_effector_right,
+                          "end_effector_left": launch_args.end_effector_left,
+                          "ft_sensor_right": launch_args.ft_sensor_right,
+                          "ft_sensor_left": launch_args.ft_sensor_left,
+                          "wrist_model_right": launch_args.wrist_model_right,
+                          "wrist_model_left": launch_args.wrist_model_left,
+                          "laser_model": launch_args.laser_model,
+                          "camera_model": launch_args.camera_model,
+                          "base_type": launch_args.base_type,
+                          "namespace": launch_args.namespace,
+                          "use_sim_time": launch_args.use_sim_time
+                          })
+
+    launch_description.add_action(robot_state_publisher)
+
+    joint_state_pub_gui = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui',
         output='screen')
 
+    launch_description.add_action(joint_state_pub_gui)
+
     rviz_config_file = PathJoinSubstitution(
         [FindPackageShare('tiago_pro_description'), 'config', 'show.rviz'])
 
-    start_rviz_cmd = Node(
+    rviz = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         arguments=['-d', rviz_config_file],
         output='screen',
         parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')
-                     }]
-    )
+                     }])
+    launch_description.add_action(rviz)
 
+    return
+
+
+def generate_launch_description():
+
+    # Create the launch description
     ld = LaunchDescription()
 
-    ld.add_action(robot_state_publisher)
-    ld.add_action(start_joint_pub_gui)
-    ld.add_action(start_rviz_cmd)
+    launch_arguments = LaunchArguments()
+
+    launch_arguments.add_to_launch_description(ld)
+
+    declare_actions(ld, launch_arguments)
 
     return ld
