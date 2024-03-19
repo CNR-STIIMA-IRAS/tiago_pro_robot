378450fd1af74195d007e40d5bda04b909708df0
David ter Kuile <david.terkuile@pal-robotics.com>
2024-03-19 10:08:46 +0100

    Remove unused imports for flake test

--- tiago_pro_controller_configuration/launch/default_controllers.launch.py
+++ tiago_pro_controller_configuration/launch/default_controllers.launch.py
@@ -17,7 +17,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import OpaqueFunction, GroupAction
-from launch.conditions import LaunchConfigurationEquals, LaunchConfigurationNotEquals, IfCondition
+from launch.conditions import LaunchConfigurationNotEquals, IfCondition
 from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_pal.param_utils import merge_param_files
 from launch.actions import DeclareLaunchArgument
