dd1491d0e85d3d827d8d9b7f13e0184b74305362
David ter Kuile <david.terkuile@pal-robotics.com>
2024-03-20 14:15:34 +0100

    Add missnig use sim time arg

--- tiago_pro_bringup/launch/tiago_pro_bringup.launch.py
+++ tiago_pro_bringup/launch/tiago_pro_bringup.launch.py
@@ -50,6 +50,7 @@ def declare_actions(launch_description: LaunchDescription, launch_args: LaunchAr
                           "end_effector_left": launch_args.end_effector_left,
                           "ft_sensor_right": launch_args.ft_sensor_right,
                           "ft_sensor_left": launch_args.ft_sensor_left,
+                          "use_sim_time": launch_args.use_sim_time
                           })
 
     launch_description.add_action(default_controllers)
