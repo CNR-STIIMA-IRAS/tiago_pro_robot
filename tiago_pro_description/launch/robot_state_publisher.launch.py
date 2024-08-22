a905e4513a2e13abc25e3876b89e487854ff50de
oscarmartinez <oscar.martinez@pal-robotics.com>
2024-08-22 12:32:20 +0200

    Support for removable tray

--- tiago_pro_description/launch/robot_state_publisher.launch.py
+++ tiago_pro_description/launch/robot_state_publisher.launch.py
@@ -43,6 +43,7 @@ class LaunchArguments(LaunchArgumentsBase):
     wrist_model_left: DeclareLaunchArgument = TiagoProArgs.wrist_model_left
     camera_model: DeclareLaunchArgument = TiagoProArgs.camera_model
     laser_model: DeclareLaunchArgument = TiagoProArgs.laser_model
+    has_laptop_tray: DeclareLaunchArgument = TiagoProArgs.has_laptop_tray
     use_sim_time: DeclareLaunchArgument = CommonArgs.use_sim_time
     namespace: DeclareLaunchArgument = CommonArgs.namespace
     is_public_sim: DeclareLaunchArgument = CommonArgs.is_public_sim
@@ -82,6 +83,7 @@ def create_robot_description_param(context, *args, **kwargs):
         'wrist_model_left': read_launch_argument('wrist_model_left', context),
         'camera_model': read_launch_argument('camera_model', context),
         'laser_model': read_launch_argument('laser_model', context),
+        'has_laptop_tray': read_launch_argument('has_laptop_tray', context),
         'use_sim_time': read_launch_argument('use_sim_time', context),
         'namespace': read_launch_argument('namespace', context),
         'is_public_sim': read_launch_argument('is_public_sim', context),
