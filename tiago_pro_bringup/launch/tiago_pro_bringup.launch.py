3a2234fc33f8105b62370a0223db3cc8d3262272
oscarmartinez <oscar.martinez@pal-robotics.com>
2024-08-23 15:40:23 +0200

    Remove the unnecesary ft_sensor arguments in bringup

--- tiago_pro_bringup/launch/tiago_pro_bringup.launch.py
+++ tiago_pro_bringup/launch/tiago_pro_bringup.launch.py
@@ -65,8 +65,6 @@ def declare_actions(launch_description: LaunchDescription, launch_args: LaunchAr
                           "arm_type_left": launch_args.arm_type_left,
                           "end_effector_right": launch_args.end_effector_right,
                           "end_effector_left": launch_args.end_effector_left,
-                          "ft_sensor_right": launch_args.ft_sensor_right,
-                          "ft_sensor_left": launch_args.ft_sensor_left,
                           "use_sim_time": launch_args.use_sim_time})
 
     launch_description.add_action(play_motion2)
