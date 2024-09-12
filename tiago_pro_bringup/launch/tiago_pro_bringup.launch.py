bf4d5af2a2d6f6d40085c4f18f2e2493e87d2716
oscarmartinez <oscar.martinez@pal-robotics.com>
2024-09-12 10:59:03 +0200

    New launch args

--- tiago_pro_bringup/launch/tiago_pro_bringup.launch.py
+++ tiago_pro_bringup/launch/tiago_pro_bringup.launch.py
@@ -35,6 +35,8 @@ class LaunchArguments(LaunchArgumentsBase):
     ft_sensor_left: DeclareLaunchArgument = TiagoProArgs.ft_sensor_left
     wrist_model_right: DeclareLaunchArgument = TiagoProArgs.wrist_model_right
     wrist_model_left: DeclareLaunchArgument = TiagoProArgs.wrist_model_left
+    tool_changer_right: DeclareLaunchArgument = TiagoProArgs.tool_changer_right
+    tool_changer_left: DeclareLaunchArgument = TiagoProArgs.tool_changer_left
     camera_model: DeclareLaunchArgument = TiagoProArgs.camera_model
     laser_model: DeclareLaunchArgument = TiagoProArgs.laser_model
     has_laptop_tray: DeclareLaunchArgument = TiagoProArgs.has_laptop_tray
@@ -89,6 +91,8 @@ def declare_actions(launch_description: LaunchDescription, launch_args: LaunchAr
                           "ft_sensor_left": launch_args.ft_sensor_left,
                           "wrist_model_right": launch_args.wrist_model_right,
                           "wrist_model_left": launch_args.wrist_model_left,
+                          "tool_changer_right": launch_args.tool_changer_right,
+                          "tool_changer_left": launch_args.tool_changer_left,
                           "laser_model": launch_args.laser_model,
                           "camera_model": launch_args.camera_model,
                           "base_type": launch_args.base_type,
