13df09e57201546fc8ae7b86170adc3721d233d8
Aina <aina.irisarri@pal-robotics.com>
2024-11-11 14:26:31 +0100

    Remove camera model as arg

--- tiago_pro_bringup/launch/tiago_pro_bringup.launch.py
+++ tiago_pro_bringup/launch/tiago_pro_bringup.launch.py
@@ -37,7 +37,6 @@ class LaunchArguments(LaunchArgumentsBase):
     wrist_model_left: DeclareLaunchArgument = TiagoProArgs.wrist_model_left
     tool_changer_right: DeclareLaunchArgument = TiagoProArgs.tool_changer_right
     tool_changer_left: DeclareLaunchArgument = TiagoProArgs.tool_changer_left
-    # camera_model: DeclareLaunchArgument = TiagoProArgs.camera_model
     laser_model: DeclareLaunchArgument = TiagoProArgs.laser_model
     has_laptop_tray: DeclareLaunchArgument = TiagoProArgs.has_laptop_tray
     use_sim_time: DeclareLaunchArgument = CommonArgs.use_sim_time
@@ -96,7 +95,6 @@ def declare_actions(launch_description: LaunchDescription, launch_args: LaunchAr
                           "tool_changer_right": launch_args.tool_changer_right,
                           "tool_changer_left": launch_args.tool_changer_left,
                           "laser_model": launch_args.laser_model,
-                          #   "camera_model": launch_args.camera_model,
                           "base_type": launch_args.base_type,
                           "has_laptop_tray": launch_args.has_laptop_tray,
                           "namespace": launch_args.namespace,
