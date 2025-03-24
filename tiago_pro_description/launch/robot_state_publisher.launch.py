32461ad7cdd36d8fc887a423f200a133f3cbeae3
oscarmartinez <oscar.martinez@pal-robotics.com>
2025-03-24 16:35:55 +0100

    Support for SEA in tiago_pro_description

--- tiago_pro_description/launch/robot_state_publisher.launch.py
+++ tiago_pro_description/launch/robot_state_publisher.launch.py
@@ -45,6 +45,7 @@ class LaunchArguments(LaunchArgumentsBase):
     laser_model: DeclareLaunchArgument = TiagoProArgs.laser_model
     tool_changer_right: DeclareLaunchArgument = TiagoProArgs.tool_changer_right
     tool_changer_left: DeclareLaunchArgument = TiagoProArgs.tool_changer_left
+    torque_estimation: DeclareLaunchArgument = TiagoProArgs.torque_estimation
     use_sim_time: DeclareLaunchArgument = CommonArgs.use_sim_time
     namespace: DeclareLaunchArgument = CommonArgs.namespace
     is_public_sim: DeclareLaunchArgument = CommonArgs.is_public_sim
@@ -86,6 +87,7 @@ def create_robot_description_param(context, *args, **kwargs):
         'laser_model': read_launch_argument('laser_model', context),
         'tool_changer_right': read_launch_argument('tool_changer_right', context),
         'tool_changer_left': read_launch_argument('tool_changer_left', context),
+        'torque_estimation': read_launch_argument('torque_estimation', context),
         'use_sim_time': read_launch_argument('use_sim_time', context),
         'namespace': read_launch_argument('namespace', context),
         'is_public_sim': read_launch_argument('is_public_sim', context),
