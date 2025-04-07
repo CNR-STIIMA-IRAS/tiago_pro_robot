a44f2aa42f63ee2b764f5d9d6a8f422cba19c335
ileniaperrella <ilenia.perrella@pal-robotics.com>
2025-04-07 12:33:53 +0200

    fix name from triago to tiagopro

--- tiago_pro_controller_configuration/launch/tsid_default_controllers.launch.py
+++ tiago_pro_controller_configuration/launch/tsid_default_controllers.launch.py
@@ -22,15 +22,15 @@ from controller_manager.launch_utils import generate_load_controller_launch_desc
 from launch_pal.arg_utils import LaunchArgumentsBase, read_launch_argument
 from launch_pal.param_utils import parse_parametric_yaml, merge_param_files
 from launch_pal.robot_arguments import CommonArgs
-from triago_description.launch_arguments import TriagoArgs
+from tiago_pro_description.launch_arguments import TiagoProArgs
 
 from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
 class LaunchArguments(LaunchArgumentsBase):
-    arm_type_right: DeclareLaunchArgument = TriagoArgs.arm_type_right
-    arm_type_left: DeclareLaunchArgument = TriagoArgs.arm_type_left
+    arm_type_right: DeclareLaunchArgument = TiagoProArgs.arm_type_right
+    arm_type_left: DeclareLaunchArgument = TiagoProArgs.arm_type_left
     use_sim_time: DeclareLaunchArgument = CommonArgs.use_sim_time
     namespace: DeclareLaunchArgument = CommonArgs.namespace
 
@@ -73,7 +73,7 @@ def setup_torso_controllers(context, *args, **kwargs):
 
 def setup_torso_controller(context, controller_name, load_gains_separately=False):
     param_file = os.path.join(
-        get_package_share_directory("triago_controller_configuration"),
+        get_package_share_directory("tiago_pro_controller_configuration"),
         "config",
         "tsid",
         f"{controller_name}.yaml",
@@ -85,7 +85,7 @@ def setup_torso_controller(context, controller_name, load_gains_separately=False
         sim_postfix = "_sim" if use_sim_time == "True" else ""
 
         gains_file = os.path.join(
-            get_package_share_directory("triago_controller_configuration"),
+            get_package_share_directory("tiago_pro_controller_configuration"),
             "config",
             "tsid",
             "gains",
@@ -157,7 +157,7 @@ def setup_arm_side_controller(
     remappings = {"ARM_SIDE_PREFIX": arm_prefix}
 
     param_file = os.path.join(
-        get_package_share_directory("triago_controller_configuration"),
+        get_package_share_directory("tiago_pro_controller_configuration"),
         "config",
         "tsid",
         f"{controller_name}.yaml",
@@ -172,7 +172,7 @@ def setup_arm_side_controller(
         sim_postfix = "_sim" if use_sim_time == "True" else ""
 
         gains_file = os.path.join(
-            get_package_share_directory("triago_controller_configuration"),
+            get_package_share_directory("tiago_pro_controller_configuration"),
             "config",
             "tsid",
             "gains",
