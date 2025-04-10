0c372d76a4f54c620c083dc22afc98f1e72f711e
oscarmartinez <oscar.martinez@pal-robotics.com>
2025-04-10 15:20:12 +0200

    Launching the inertia shaping controllers if torque_estimation enabled

--- tiago_pro_controller_configuration/launch/default_controllers.launch.py
+++ tiago_pro_controller_configuration/launch/default_controllers.launch.py
@@ -17,7 +17,7 @@ from typing import List
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import GroupAction
-from launch.conditions import LaunchConfigurationNotEquals, IfCondition, UnlessCondition
+from launch.conditions import LaunchConfigurationNotEquals, LaunchConfigurationEquals, IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
 from controller_manager.launch_utils import generate_load_controller_launch_description
@@ -134,6 +134,12 @@ def configure_side_controllers(context, end_effector_side='right', *args, **kwar
         condition=UnlessCondition(LaunchConfiguration("is_public_sim")))
     use_sim_time = read_launch_argument("use_sim_time", context)
 
+    inertia_shaping_controllers = include_scoped_launch_py_description(
+        pkg_name='pal_sea_arm_controller_configuration',
+        paths=['launch', 'inertia_shaping_controllers.launch.py'],
+        launch_arguments={"side": end_effector_side},
+        condition=IfCondition(LaunchConfiguration("torque_estimation")))
+
     end_effector = read_launch_argument(end_effector_arg_name, context)
     end_effector_underscore = end_effector.replace('-', '_')
 
@@ -165,7 +171,7 @@ def configure_side_controllers(context, end_effector_side='right', *args, **kwar
     )
 
     return [arm_controller, gravity_compensation_controller,
-            end_effector_controller, ft_sensor_controller]
+            inertia_shaping_controllers, end_effector_controller, ft_sensor_controller]
 
 
 def concatenate_strings(strings: List[str], delimiter: str = '', skip_empty: bool = False):
